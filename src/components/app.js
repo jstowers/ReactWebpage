f93f3f1d4c8579d4b3c35b6bf761b13af14f1516
Joe Stowers <greenchiletexan@gmail.com>
2017-12-10 02:39:45 -0600

    webpack.config: fix publicPath so webpage can reload automatically on changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   	render() {
 	    return (
 	      	<div>
-	      		<h1>My Updated Webpage</h1>
+	      		<h1>Joe Stowers</h1>
   			</div>
 	    );
   	}
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
+
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-//import reducers from './reducers';
-
-//const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+// import reducers from './reducers';
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App/>
-  , document.querySelector('.container'));
+	<App />
+	, document.querySelector('.container'));
 
-// //<Provider store={createStoreWithMiddleware(reducers)}>
+
+//<Provider store={createStoreWithMiddleware(reducers)}>
 //</Provider>
